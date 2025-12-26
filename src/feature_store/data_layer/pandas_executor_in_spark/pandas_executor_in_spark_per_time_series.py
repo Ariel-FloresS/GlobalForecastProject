f241c4c828cda06b923635846b0e7256f1874621
Ariel <arielfloressantacruz3@gmail.com>
2025-12-25 20:27:45 -0600

    feat(pandas_executor_in_spark): Add this module to execute pandas functions in a spark dataframe

---
+++ src/feature_store/data_layer/pandas_executor_in_spark/pandas_executor_in_spark_per_time_series.py
@@ -0,0 +1,21 @@
+from .pandas_executor_in_spark_per_time_series_interface import PandasExecutorInSparkPerTimeSeriesInterface
+from pyspark.sql import DataFrame
+import pandas as pd
+from typing import Callable
+
+
+class PandasExecutorInSparkPerTimeSeries(PandasExecutorInSparkPerTimeSeriesInterface):
+
+
+    def apply_per_series(spark_dataframe_to_apply_the_pandas_function:DataFrame,
+                        function: Callable[[pd.DataFrame], pd.DataFrame],
+                        output_schema:str)->DataFrame:
+        
+
+        return (
+            spark_dataframe_to_apply_the_pandas_function
+            .groupBy('unique_id')
+            .applyInPandas(func = function,
+                           schema = output_schema)
+        )
+
