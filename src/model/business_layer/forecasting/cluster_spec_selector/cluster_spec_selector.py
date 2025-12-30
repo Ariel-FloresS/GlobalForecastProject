dd8267f63ff7b9d1f59b37a96c9d78859d3a1980
Ariel <arielfloressantacruz3@gmail.com>
2025-12-30 11:33:52 -0600

    feat(cluster_spec_selector): Add cluster_spec_selector module for get the ModelSpec for cluster/classification

---
+++ src/model/business_layer/forecasting/cluster_spec_selector/cluster_spec_selector.py
@@ -0,0 +1,19 @@
+from forecasting.cluster_spec_selector.cluter_spec_selector_interface import ClusterSpecSelectorInterface
+from forecasting.config import ModelSpec, MODEL_SPECS_BY_CLASS
+from typing import Dict
+
+class ClusterSpecSelector(ClusterSpecSelectorInterface):
+
+    def __init__(self, model_specs_by_class:Dict[str, ModelSpec] = MODEL_SPECS_BY_CLASS)->None:
+
+        self._model_specs_by_class = model_specs_by_class
+
+    def get_spec_by_classification(self, classification:str)->ModelSpec:
+        
+        if classification not in self._model_specs_by_class.keys():
+            raise ValueError(f"Unknow classification = '{classification}'. Allowed: {self._model_specs_by_class.keys()}")
+        
+        return self._model_specs_by_class[classification] 
+
+
+
