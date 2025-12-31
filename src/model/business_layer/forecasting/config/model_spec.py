80bfb4ffd530f7048d5c49d5ceaa70812226d079
Ariel <arielfloressantacruz3@gmail.com>
2025-12-31 15:37:48 -0600

    feat(config):Add new atributes for the ModelSpec

--- src/model/business_layer/forecasting/config/model_spec.py
+++ src/model/business_layer/forecasting/config/model_spec.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass, field
-from typing import Dict, Any
+from typing import Dict, Any, List, Optional
 
 @dataclass(frozen = True)
 class ModelSpec:
     name:str
-    models:Dict[str, Dict[str, Any]] = field(default = dict)
+    lags: Optional[List[int]] = None
+    lag_transforms: Optional[Dict[int, List[Any]]] = None
+    target_transforms: Optional[List[Any] ]= None
+    models:Dict[str, Dict[str, Any]] = field(default_factory = dict)
