84c788aae40172a2b09dffdef29b7de638b58125
Ariel <arielfloressantacruz3@gmail.com>
2026-01-12 18:09:11 -0600

    feat(config): config folder for setting up the imputation steps

---
+++ src/data/business_layer/imputation/config/policy_file.py
@@ -0,0 +1,9 @@
+from imputation.imputers import ImputerInterface, RollingMeanFFillImputer, RollingMedianFFillImputer, ZeroFillImputer
+from typing import Dict
+
+IMPUTER_BY_CLASS: Dict[str, ImputerInterface] = {
+    'Smooth': RollingMeanFFillImputer(window_size = 3),
+    'Erratic': RollingMedianFFillImputer(window_size = 4),
+    'Intermittent': ZeroFillImputer(),
+    'Lumpy':RollingMedianFFillImputer(window_size = 12)
+}
