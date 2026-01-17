68bb215cbc70ccdecf231414605a0e495454bf8b
Ariel <arielfloressantacruz3@gmail.com>
2026-01-16 20:24:09 -0600

    feat(train_test_future_dataset): adding the future for spliting the time series and get the training , test and future dataset

--- src/data/presentation_layer/data_pipeline.py
+++ src/data/presentation_layer/data_pipeline.py
@@ -11,8 +11,11 @@ from data.business_layer.classification import DemanClassifierFrepple
 from data.business_layer.imputation.segmented_imputation_pipeline import SegmentedImputationPipeline
 from data.business_layer.data_preparation_service import DataPreparationSeviceInterface, DataPreparationService
 from feature_store.presentation_layer import FeatureStoreInterface, FeatureStore
-from pyspark.sql import DataFrame, SparkSession
-from typing import List
+from pyspark.sql import DataFrame, SparkSession, Row
+import pyspark.sql.functions as F
+from typing import List, Optional, Tuple
+from loguru import logger
+
 
 
 class DataPipeline(DataPipelineInterface):
@@ -20,7 +23,8 @@ class DataPipeline(DataPipelineInterface):
     def __init__(self, spark: SparkSession, raw_delta_table: str, id_column:str,
                 time_column:str, target_column:str, frequency:str, season_length:int,
                 inactivity_periods:int, min_records_in_time_series: int, exogenous_columns:List[str],
-                training_delta_table:str)->None:
+                training_delta_table:str,
+                static_features:Optional[List[str]] = None)->None:
 
         self.spark = spark
         self.raw_delta_table = raw_delta_table
@@ -33,15 +37,11 @@ class DataPipeline(DataPipelineInterface):
         self.min_records_in_time_series = min_records_in_time_series
         self.exogenous_columns = exogenous_columns
         self.training_delta_table = training_delta_table
+        self.static_features = static_features
 
-    def run_pipeline(self)->DataFrame:
 
-        raw_data_repository: RawDataRepositoryInterface = RawDataRepository(spark = self.spark)
+    def _generate_cleaning_dataset(self, raw_dataset:DataFrame)->DataFrame:
 
-        raw_dataset: DataFrame = raw_data_repository.load_raw_data(delta = self.raw_delta_table,
-                                                                id_column = self.id_column,
-                                                                time_column = self.time_column,
-                                                                target_column = self.target_column)
         data_cleaning_steps: List[DataCleaningStepInterface] = [
             FillMissingDatesStep(spark = self.spark, frequency = self.frequency),
             RemoveLeadingNullsStep(),
@@ -59,6 +59,29 @@ class DataPipeline(DataPipelineInterface):
 
         cleaning_dataset: DataFrame = data_preparation_Service.data_prepare(raw_dataset = raw_dataset)
 
+        return cleaning_dataset
+
+
+    def run_pipeline(self)->DataFrame:
+
+        pipeline_name: str = self.__class__.__name__
+
+        banner_top: str = f"\n{'='*84}\n[DATA PIPELINE START]   {pipeline_name}\n{'='*84}"
+        logger.info(banner_top)
+
+        raw_data_repository: RawDataRepositoryInterface = RawDataRepository(spark = self.spark)
+
+        raw_dataset: DataFrame = raw_data_repository.load_raw_data(delta = self.raw_delta_table,
+                                                                id_column = self.id_column,
+                                                                time_column = self.time_column,
+                                                                target_column = self.target_column,
+                                                                static_features = self.static_features)
+        
+
+        
+        cleaning_dataset: DataFrame = self._generate_cleaning_dataset(raw_dataset = raw_dataset)
+        
+
         feature_store: FeatureStoreInterface = FeatureStore(spark = self.spark, 
                                                             frequency = self.frequency,
                                                             season_length = self.season_length
@@ -69,6 +92,84 @@ class DataPipeline(DataPipelineInterface):
         TrainingDataRepository().save_training_data(training_dataframe = training_dataset,
                                                     delta = self.training_delta_table,
                                                     exogenous_columns = self.exogenous_columns)
+        
+
+        banner_end: str = f"\n{'='*84}\n[DATA PIPELINE END]   {pipeline_name}\n{'='*84}"
+        logger.info(banner_end)
+        
+    def train_test_future_datasets_split(self, split_date:str, horizon:int)->Tuple[DataFrame, DataFrame, DataFrame]:
+
+        pipeline_name: str = self.__class__.__name__
+
+        banner_top: str = f"\n{'='*84}\n[TRAIN, TEST, FUTURE SPLIT START]   {pipeline_name}\n{'='*84}"
+        logger.info(banner_top)
+
+        raw_data_repository: RawDataRepositoryInterface = RawDataRepository(spark = self.spark)
+
+        train_dataset_raw, test_dataset_raw = raw_data_repository.train_test_split(split_date = split_date,
+                                                                                    delta = self.raw_delta_table,
+                                                                                    id_column = self.id_column,
+                                                                                    time_column = self.time_column,
+                                                                                    target_column = self.target_column,
+                                                                                    static_features = self.static_features )
+        
+        train_cleaning_dataset: DataFrame = self._generate_cleaning_dataset(raw_dataset = train_dataset_raw)
+
+        feature_store: FeatureStoreInterface = FeatureStore(spark = self.spark, 
+                                                            frequency = self.frequency,
+                                                            season_length = self.season_length
+                                                            )
+        
+        training_dataset: DataFrame = feature_store.train_dataset(historical = train_cleaning_dataset)
+
+        future_dataset: DataFrame = feature_store.future_dataset(historical = train_cleaning_dataset, horizon = horizon)
+
+
+        bounds_train: Row = (
+            training_dataset
+            .select(F.min("ds").alias("min_ds"), F.max("ds").alias("max_ds"))
+            .first()
+        )
+        bounds_test: Row = (
+            test_dataset_raw
+            .select(F.min("ds").alias("min_ds"), F.max("ds").alias("max_ds"))
+            .first()
+        )
+
+        bounds_future: Row = (
+            future_dataset
+            .select(F.min("ds").alias("min_ds"), F.max("ds").alias("max_ds"))
+            .first()
+        )
+
+
+        logger.info(
+            f'[{pipeline_name}] Dataset ranges:\n'
+            f'  • TRAIN  | min_ds={bounds_train["min_ds"]} | max_ds={bounds_train["max_ds"]}\n'
+            f'  • TEST   | min_ds={bounds_test["min_ds"]} | max_ds={bounds_test["max_ds"]}\n'
+            f'  • FUTURE | min_ds={bounds_future["min_ds"]} | max_ds={bounds_future["max_ds"]}'
+        )
+
+        banner_end: str = (
+            f'\n{"="*84}\n'
+            f'[PIPELINE END] TRAIN / TEST / FUTURE SPLIT | {pipeline_name}\n'
+            f'{"="*84}'
+        )
+
+        logger.info(banner_end)
+
+
+        return training_dataset, test_dataset_raw, future_dataset
+
+
+
+        
+
+
+
+        
+        
+    
 
         
 
--- src/data/presentation_layer/data_pipeline_interface.py
+++ src/data/presentation_layer/data_pipeline_interface.py
@@ -1,9 +1,13 @@
 from abc import ABC, abstractmethod
 from pyspark.sql import DataFrame
+from typing import Tuple
 
 class DataPipelineInterface(ABC):
 
     @abstractmethod
     def run_pipeline(self)->DataFrame:
+        raise NotImplementedError
+    
+    def train_test_future_datasets_split(self, split_date:str, horizon:int)->Tuple[DataFrame, DataFrame, DataFrame]:
         raise NotImplementedError
 
