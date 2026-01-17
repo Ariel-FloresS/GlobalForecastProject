11325ccda07449c312649b17938d9e41ac5858e4
Ariel <arielfloressantacruz3@gmail.com>
2026-01-16 23:55:25 -0600

    fix(generete_future_dataset_interface): forget static_features in the interface

--- src/feature_store/business_layer/future_dataset/generate_future_dataset_interface.py
+++ src/feature_store/business_layer/future_dataset/generate_future_dataset_interface.py
@@ -1,13 +1,13 @@
 from abc import ABC, abstractmethod
 from pyspark.sql import DataFrame, SparkSession
-
+from typing import Optional, List
 
 
 class GenerateFutureDatasetInterface(ABC):
 
     @abstractmethod
-    def generate_dataset(self, spark:SparkSession ,historical_dataframe:DataFrame,
-                        horizon:int, frequency:str)->DataFrame:
+    def generate_dataset(self, spark: SparkSession, historical_dataframe:DataFrame,
+                        horizon:int, frequency:str , static_features:Optional[List[str]] = None)->DataFrame:
         
         raise NotImplementedError
 
