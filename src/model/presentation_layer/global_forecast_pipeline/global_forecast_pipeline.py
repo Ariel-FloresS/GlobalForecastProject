397a29d059a6a60e0df2280fa4df44b096446b83
Ariel <arielfloressantacruz3@gmail.com>
2026-01-19 21:24:20 -0600

    feat(global_forecast_pipeline): backtesting feature add

--- src/model/presentation_layer/global_forecast_pipeline/global_forecast_pipeline.py
+++ src/model/presentation_layer/global_forecast_pipeline/global_forecast_pipeline.py
@@ -10,7 +10,7 @@ from feature_store.presentation_layer import  FeatureStore
 from pyspark.sql import DataFrame, SparkSession
 from typing import List, Optional
 from loguru import logger
-
+import pyspark.sql.functions as F
 
 class GlobalForecastPipeline(GlobalForecastPipelineInterface):
 
@@ -19,6 +19,27 @@ class GlobalForecastPipeline(GlobalForecastPipelineInterface):
         self.training_delta_table = training_delta_table
         self.exogenous_columns = exogenous_columns
         self.forecast_data_delta_table = forecast_data_delta_table
+
+
+    def _forecast_one_classification(self,
+                                    training_dataset: DataFrame, 
+                                    future_dataset:DataFrame,
+                                    classification: str,
+                                    frequency:str, horizon:int,
+                                    static_features:Optional[List[str]] = None)->DataFrame:
+       
+       segmented_forecast: SegmentedForecastOrchestatorInterface = SegmentedForecastOrchestator(classification = classification, 
+                                                                                                cluster_spec_selector = ClusterSpecSelector(),
+                                                                                                model_factory = ModelFactory())
+       return segmented_forecast.forecast(training_dataset = training_dataset,
+                                          future_dataset = future_dataset, 
+                                          frequency = frequency,
+                                          horizon = horizon,
+                                          static_features = static_features)
+       
+       
+
+       
 
     def forecast(self ,frequency:str, season_lenght:int, horizon:int, version:str ,static_features:Optional[List[str]] = None)->None:
         
@@ -39,50 +60,36 @@ class GlobalForecastPipeline(GlobalForecastPipelineInterface):
                                                                                                                 exogenous_columns = self.exogenous_columns,
                                                                                                                 horizon = horizon,
                                                                                                                 static_features = static_features)
+      
         
-        smooth_segmented_forecast: SegmentedForecastOrchestatorInterface = SegmentedForecastOrchestator(classification = 'Smooth', 
-                                                                                                        cluster_spec_selector = ClusterSpecSelector(),
-                                                                                                        model_factory = ModelFactory())
-        
-        erratic_segmented_forecast: SegmentedForecastOrchestatorInterface = SegmentedForecastOrchestator(classification = 'Erratic',
-                                                                                                        cluster_spec_selector = ClusterSpecSelector(),
-                                                                                                        model_factory = ModelFactory()
-                                                                                                        )
-        
-        lumpy_segmente_forecast: SegmentedForecastOrchestatorInterface = SegmentedForecastOrchestator(classification = 'Lumpy',
-                                                                                                    cluster_spec_selector = ClusterSpecSelector(),
-                                                                                                    model_factory = ModelFactory()
-                                                                                                      )
-        
-        intermittent_segmented_forecast: SegmentedForecastOrchestatorInterface = SegmentedForecastOrchestator(classification =  'Intermittent',
-                                                                                                              cluster_spec_selector = ClusterSpecSelector(),
-                                                                                                                model_factory = ModelFactory()
-                                                                                                              )
-        
-        smooth_predictions: DataFrame = smooth_segmented_forecast.forecast(training_dataset = training_dataset,
-                                                                            future_dataset = future_dataset, 
-                                                                            frequency = frequency,
-                                                                            horizon = horizon,
-                                                                            static_features = static_features)
-        
-        erratic_predictions: DataFrame = erratic_segmented_forecast.forecast(training_dataset = training_dataset,
-                                                                             future_dataset = future_dataset,
-                                                                             frequency = frequency,
-                                                                             horizon = horizon,
-                                                                             static_features = static_features)
-        
-        
-        lumpy_predictions: DataFrame = lumpy_segmente_forecast.forecast(training_dataset = training_dataset,
+        smooth_predictions: DataFrame = self._forecast_one_classification(training_dataset = training_dataset,
                                                                         future_dataset = future_dataset,
+                                                                        classification = 'Smooth',
                                                                         frequency = frequency,
-                                                                        horizon =  horizon,
+                                                                        horizon = horizon,
                                                                         static_features = static_features)
         
-        intermittent_predictions: DataFrame = intermittent_segmented_forecast.forecast(training_dataset = training_dataset,
-                                                                                       future_dataset = future_dataset,
-                                                                                       frequency = frequency,
-                                                                                       horizon = horizon,
-                                                                                       static_features = static_features)
+        erratic_predictions: DataFrame = self._forecast_one_classification(training_dataset = training_dataset,
+                                                                        future_dataset = future_dataset,
+                                                                        classification = 'Erratic',
+                                                                        frequency = frequency,
+                                                                        horizon = horizon,
+                                                                        static_features = static_features)
+        
+        
+        lumpy_predictions: DataFrame = self._forecast_one_classification(training_dataset = training_dataset,
+                                                                        future_dataset = future_dataset,
+                                                                        classification = 'Lumpy',
+                                                                        frequency = frequency,
+                                                                        horizon = horizon,
+                                                                        static_features = static_features)
+        
+        intermittent_predictions: DataFrame = self._forecast_one_classification(training_dataset = training_dataset,
+                                                                        future_dataset = future_dataset,
+                                                                        classification = 'Intermittent',
+                                                                        frequency = frequency,
+                                                                        horizon = horizon,
+                                                                        static_features = static_features)
         
         prediction_dataframe_output: DataFrame = (smooth_predictions
                                                   .unionByName(erratic_predictions)
@@ -97,6 +104,83 @@ class GlobalForecastPipeline(GlobalForecastPipelineInterface):
         banner_end: str = f"\n{'='*84}\n[FORECAST PIPELINE END]   {pipeline_name}\n{'='*84}"
         logger.info(banner_end)
     
+    def _backtesting_one_classification(
+                    self,
+                    training_dataset:DataFrame,
+                    frequency:str,
+                    classification:str,
+                    windows:int,
+                    periods_for_each_window:int,
+                    static_features:Optional[List[str]] = None)->DataFrame:
+      
+      
+        
+      segmented_forecast: SegmentedForecastOrchestatorInterface = SegmentedForecastOrchestator(classification = classification, 
+                                                                                              cluster_spec_selector = ClusterSpecSelector(),
+                                                                                              model_factory = ModelFactory())
+      return segmented_forecast.cross_validation(training_dataset = training_dataset,
+                                          frequency = frequency,
+                                          windows = windows,
+                                          periods_for_each_window = periods_for_each_window,
+                                          static_features = static_features)
+        
+    def backtesting(
+                    self,
+                    training_dataset:DataFrame,
+                    frequency:str,
+                    windows:int,
+                    periods_for_each_window:int,
+                    static_features:Optional[List[str]] = None)->DataFrame:
+      
+      pipeline_name: str = self.__class__.__name__
+
+      banner_top: str = f"\n{'='*84}\n[BACKTESTING PIPELINE START]   {pipeline_name}\n{'='*84}"
+      logger.info(banner_top)
+        
+      smooth_cross_validation_dataframe: DataFrame = self._backtesting_one_classification(training_dataset = training_dataset,
+                                                                                          frequency = frequency,
+                                                                                          classification = 'Smooth',
+                                                                                          windows = windows,
+                                                                                          periods_for_each_window = periods_for_each_window,
+                                                                                          static_features = static_features)
+      
+      erratic_cross_validation_dataframe: DataFrame = self._backtesting_one_classification(training_dataset = training_dataset,
+                                                                                          frequency = frequency,
+                                                                                          classification = 'Erratic',
+                                                                                          windows = windows,
+                                                                                          periods_for_each_window = periods_for_each_window,
+                                                                                          static_features = static_features)
+      
+      lumpy_cross_validation_dataframe: DataFrame = self._backtesting_one_classification(training_dataset = training_dataset,
+                                                                                          frequency = frequency,
+                                                                                          classification = 'Lumpy',
+                                                                                          windows = windows,
+                                                                                          periods_for_each_window = periods_for_each_window,
+                                                                                          static_features = static_features)
+      
+      intermittent_cross_validation_dataframe: DataFrame = self._backtesting_one_classification(training_dataset = training_dataset,
+                                                                                          frequency = frequency,
+                                                                                          classification = 'Intermittent',
+                                                                                          windows = windows,
+                                                                                          periods_for_each_window = periods_for_each_window,
+                                                                                          static_features = static_features)
+      backtesting_dataframe_output: DataFrame = (smooth_cross_validation_dataframe
+                                                  .unionByName(erratic_cross_validation_dataframe)
+                                                  .unionByName(lumpy_cross_validation_dataframe)
+                                                  .unionByName(intermittent_cross_validation_dataframe)
+                                                  )
+      
+      banner_end: str = f"\n{'='*84}\n[BACKTESTING PIPELINE END]   {pipeline_name}\n{'='*84}"
+      logger.info(banner_end)
+      
+      return backtesting_dataframe_output
+      
+      
+
+        
+      
+      
+        
 
         
 
