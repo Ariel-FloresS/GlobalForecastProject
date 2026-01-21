52aa337997822cd0c9cc6face186827fb1231ae9
Ariel <arielfloressantacruz3@gmail.com>
2026-01-21 09:35:23 -0600

    feat(model_factory): Add built_local_model method

--- src/model/business_layer/forecasting/model_factory/model_factory_interface.py
+++ src/model/business_layer/forecasting/model_factory/model_factory_interface.py
@@ -1,9 +1,14 @@
 from abc import ABC, abstractmethod
-from typing import Dict, Any, TYPE_CHECKING, List
+from typing import Dict, Any, TYPE_CHECKING, List, Tuple
 from .distributed_model import DistributedModel
+from .local_model import LocalModel
 
 class ModelFactoryInterface(ABC):
 
     @abstractmethod
     def built_models(self, models_config: Dict[str, Dict[str,Any]])->List[DistributedModel]:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def built_local_model(self, distributed_model: Dict[DistributedModel])->Tuple[str,LocalModel]:
         raise NotImplementedError
